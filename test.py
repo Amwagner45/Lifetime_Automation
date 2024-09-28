97722ded38dbc1d7572eef24d240f07b9e0e4bf9
Alexander Michael Wagner <amwagner45@gmail.com>
2024-09-28 13:54:57 -0400

    added example .env file

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
 
 url = "https://www.google.com/"
 driver.get(url)
 driver.implicitly_wait(10)
 
-test_var = driver.find_element(By.XPATH, "(//*[name()='a' and @class='pHiOh'])[1]")
+# test_var = driver.find_element(By.XPATH, "(//*[name()='a' and @class='pHiOh'])[1]")
 
 # Expecting About on PC and Advertising using Selenium because of smaller window
-print(test_var.text)
+# print(test_var.text)
+print(driver.title)
